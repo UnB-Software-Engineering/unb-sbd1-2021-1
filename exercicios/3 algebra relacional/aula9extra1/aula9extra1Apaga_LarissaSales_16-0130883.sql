5a92c9ecb02618d796a7b1932ec4fd2384fe5943
LARISSA SIQUEIRA SALES <larissa.sales@embraer.com.br>
2021-10-25 23:02:28 -0300

    Add exercicios modulo 3

---
+++ exercicios/3 algebra relacional/aula9extra1/aula9extra1Apaga_LarissaSales_16-0130883.sql
@@ -0,0 +1,23 @@
+-- ---------  << Exercício Extra 1 da Aula 9 >>  -----------
+--
+--                  SCRIPT DE REMOCAO (DDL)
+--
+-- Data Criacao ...........: 27/09/2021
+-- Autor(es) ..............: Larissa Siqueira Sales
+-- Banco de Dados .........: MySQL 8.0
+-- Base de Dados (nome) ...: aula9extra1
+--
+-- Ultimas Alteracoes
+--   27/09/2021 => Criacao dos comandos de apagar as tabelas
+--
+-- PROJETO => 01 Base de Dados
+--         => 02 Tabelas
+--
+-- ---------------------------------------------------------
+
+-- BASE DE DADOS
+USE aula9extra1;
+
+-- TABELAS
+DROP TABLE CIDADE;
+DROP TABLE ESTADO; 
